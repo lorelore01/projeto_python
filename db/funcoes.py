7573fb2ac73b8a21eb4617a0a45e3d7968e9d83e
anabeatrib@gmail.com <anabeatrib@gmail.com>
2024-10-31 20:43:51 -0300

    atualizando banco de dados

--- db/funcoes.py
+++ db/funcoes.py
@@ -1,8 +1,11 @@
 import sqlite3
-connector = sqlite3.connect("consultorio.db", check_same_thread=False)
+connector = sqlite3.connect("db/consultorio.db", check_same_thread=False)
 cursor = connector.cursor()
 
 def criar_paciente(data):
-    connector.execute("INSERT INTO pacientes (nome, cpf, idade, email, telefone, pagamento, tipo_consulta, convenio) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    cursor.execute("INSERT INTO pacientes (nome, cpf, idade, email, telefone, pagamento, convenio) VALUES (?, ?, ?, ?, ?, ?, ?)",
                       (data["nome"], data["cpf"], data["idade"], data["email"], data["telefone"], data["forma-pagamento"], data["convenio"]))
     connector.commit()
+#def recuperar_pacientes():
+    #cursor.execute("SELECT * FROM pacientes")
+    #return cursor.fetchall()
